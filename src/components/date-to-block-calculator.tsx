f03bd5092b88f63a9b187c8ea34a71227b1b2ce3
MartinGbz <martin.grabarz@outlook.fr>
2024-06-08 20:23:37 +0200

    ✨ implement datetime picker

--- src/components/date-to-block-calculator.tsx
+++ src/components/date-to-block-calculator.tsx
@@ -11,6 +11,9 @@ import { LoaderCircle } from "lucide-react";
 import Result from "./result";
 import { useBlockCalculator } from "@/hook/use-block-calculator";
 import { DatePicker } from "./date-picker";
+import { DateTimePicker } from "./date-time-picker";
+import { Button } from "./ui/button";
+import { useState } from "react";
 
 export type Numbers = {
   blockNumber: bigint | undefined;
@@ -19,6 +22,7 @@ export type Numbers = {
 
 export default function DateToBlockCalculator() {
   const { result, isLoading, dateToBlock } = useBlockCalculator();
+  const [date, setDate] = useState<Date | undefined>();
 
   return (
     <Card className="w-fit h-fit max-w-[500px] min-w-[300px]">
@@ -30,14 +34,24 @@ export default function DateToBlockCalculator() {
       </CardHeader>
       <CardContent className="flex flex-col gap-y-2">
         <div className="flex items-center gap-x-2">
-          <DatePicker
-            onChange={(date) => {
-              if (date) {
-                dateToBlock(date);
-              }
+          <DateTimePicker
+            granularity="second"
+            hourCycle={12}
+            onJsDateChange={(date) => {
+              // setDate(date);
+              console.log(date);
             }}
           />
+          {/* <DatePicker /> */}
         </div>
+        <Button
+          onClick={async () => {
+            if (date) {
+              dateToBlock(date);
+            }
+          }}>
+          {"Calculate"}
+        </Button>
         <div className="mt-2">
           {result && !isLoading && (
             <div className="flex flex-col">
