fe8677db8f8125920b354f17bff18ade745de0b8
MartinGbz <martin.grabarz@outlook.fr>
2024-08-25 01:52:04 +0200

    🐛 fix theme issues, now supporting multiple color themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
--- src/components/chain-selector.tsx
+++ src/components/chain-selector.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Select,
   SelectContent,
@@ -10,36 +12,49 @@ import { switchChain } from "wagmi/actions";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { getTheme } from "./theme-toggle";
+import { useEffect, useState } from "react";
 
 export default function ChainSelector() {
   const { theme, systemTheme, setTheme } = useTheme();
+
+  const [chainIdSelected, setChainIdSelected] = useState<string | null>(null);
+
+  useEffect(() => {
+    const chainIdSelected = localStorage.getItem("chain-id");
+    setChainIdSelected(chainIdSelected);
+  }, []);
+
   return (
-    <Select
-      defaultValue={localStorage.getItem("chain-id") ?? "1"}
-      onValueChange={async (value) => {
-        localStorage.setItem("chain-id", value);
-        const chainId = Number(value);
-        await switchChain(config, { chainId: chainId });
-        setTheme(getTheme(theme, systemTheme));
-      }}>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue />
-      </SelectTrigger>
-      <SelectContent>
-        {config.chains.map((chain) => (
-          <SelectItem key={chain.id.toString()} value={chain.id.toString()}>
-            <div className="flex gap-x-2">
-              <Image
-                src={`/chains/${chain.id}.png`}
-                alt={chain.name}
-                width={20}
-                height={20}
-              />
-              <span> {chain.name}</span>
-            </div>
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    chainIdSelected && (
+      <Select
+        defaultValue={chainIdSelected ?? "1"}
+        onValueChange={async (value) => {
+          localStorage.setItem("chain-id", value);
+
+          const chainId = Number(value);
+          await switchChain(config, { chainId: chainId });
+
+          setTheme(getTheme(theme, systemTheme));
+        }}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {config.chains.map((chain) => (
+            <SelectItem key={chain.id.toString()} value={chain.id.toString()}>
+              <div className="flex gap-x-2">
+                <Image
+                  src={`/chains/${chain.id}.png`}
+                  alt={chain.name}
+                  width={20}
+                  height={20}
+                />
+                <span> {chain.name}</span>
+              </div>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    )
   );
 }
